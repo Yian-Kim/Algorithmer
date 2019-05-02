6e83d63d837377eb1e702d9b26d909ab2f547154
xxHANIxx <47965068+xxHANIxx@users.noreply.github.com>
2019-05-02 20:28:06 +0900

    ADD FILE [BOJ_Step1_2557.java]
    - 파일명 : BOJ_Step1_2557.java
    - 작성자 : KCM
    - 작성일자 : 2019.04.20.
    - 문제 : BOJ 1단계 입/출력 받아보기 
                2557번 Hello World (https://www.acmicpc.net/problem/2557)
    - 난이도 : ★☆☆☆☆
    - 후기 : Baekjoon Online Judge 사이트 특성상 작성한 소스코드를 제출하려면 
                반드시 클래스명을 Main으로 작성해야 함! 
                이를 알지 못해 컴파일 에러 3번이나 남...

---
+++ BOJ/KCM/BOJ_Step1_2557.java
@@ -0,0 +1,18 @@
+/*
+
+- 파일명 : BOJ_Step1_2557.java
+- 작성자 : KCM
+- 작성일자 : 2019.04.20.
+- 문제 : BOJ 1단계 입/출력 받아보기 
+        2557번 Hello World (https://www.acmicpc.net/problem/2557)
+- 난이도 : ★☆☆☆☆
+- 후기 : Baekjoon Online Judge 사이트 특성상 작성한 소스코드를 제출하려면 
+        반드시 클래스명을 Main으로 작성해야 함! 이를 알지 못해 컴파일 에러 3번이나 남...
+
+*/
+
+public class Main {
+    public static void main(String[] args){
+        System.out.print("Hello World!");
+    }
+}
