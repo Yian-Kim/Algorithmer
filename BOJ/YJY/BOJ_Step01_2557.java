133f666b43574f87e4eb25f450e75df9c3027004
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-17 22:33:14 +0900

    ADD FILE [BOJ_Step01_2557.java]
    - 파일명 : BOJ_Step01_2557.java
    - 작성자 : YJY
    - 작성일자 : 2019.05.17.
    - 문제 : BOJ 1단계 입/출력 받아보기 
            2557번 Hello World (https://www.acmicpc.net/problem/2557)

---
+++ BOJ/YJY/BOJ_Step01_2557.java
@@ -0,0 +1,17 @@
+/*
+
+- 파일명 : BOJ_Step01_2557.java
+- 작성자 : YJY
+- 작성일자 : 2019.05.17.
+- 문제 : BOJ 1단계 입/출력 받아보기 
+        2557번 Hello World (https://www.acmicpc.net/problem/2557)
+
+*/
+
+public class Main{
+    
+    public static void main(String args[]){
+        
+        System.out.println("Hello World!");
+    }       
+}
