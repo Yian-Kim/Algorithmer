cfef63bdbf761ed83e9c438a55993cd3f3e4d864
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 18:42:32 +0900

    ADD FILE [BOJ_Step3_1001.java]

---
+++ BOJ/YJY/BOJ_Step01_1001.java
@@ -0,0 +1,20 @@
+/*
+- 파일명 : BOJ_Step01_1001.java
+- 작성자 : YJY
+- 작성일자 : 2019.05.23.
+- 문제 : BOJ 1단계 입/출력 받아보기 
+        1001번 A-B (https://www.acmicpc.net/problem/1001)
+*
+
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String args[]) throws Exception{
+        
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        System.out.println(a-b);
+    }
+}
