a5c694c0da15461eb064476e4e38b1ce38c3a84a
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 19:16:22 +0900

    Update BOJ_Step07_11718.java

--- BOJ/YJY/1.입-출력 받아보기/BOJ_Step07_11718.java
+++ BOJ/YJY/1.입-출력 받아보기/BOJ_Step07_11718.java
@@ -3,7 +3,7 @@
 - 작성자 : YJY
 - 작성일자 : 2019.05.23.
 - 문제 : BOJ 7단계 입/출력 받아보기 
-        11718번 등록 (https://www.acmicpc.net/problem/11718)
+        11718번 그대로 출력하기 (https://www.acmicpc.net/problem/11718)
 */
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
