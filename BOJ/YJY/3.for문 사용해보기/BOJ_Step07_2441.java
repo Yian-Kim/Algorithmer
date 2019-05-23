057e869d94d812afb692a258112c61f7a72f7d79
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 19:38:09 +0900

    Update BOJ_Step07_2441.java

--- BOJ/YJY/3.for문 사용해보기/BOJ_Step07_2441.java
+++ BOJ/YJY/3.for문 사용해보기/BOJ_Step07_2441.java
@@ -5,3 +5,29 @@
 - 문제 : BOJ 7단계 for문 사용해보기
         2441 별 찍기 - 4 (https://www.acmicpc.net/problem/2441)
 */
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String args[]) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String a = "*";
+        String b = " ";
+        
+        int n  = Integer.parseInt(br.readLine());
+    	for(int i=0; i<n; i++) {
+    		for(int k=0; k<i; k++) {
+    			bw.write(b);
+    		}
+    		for(int j=i; j<n; j++) {
+    			bw.write(a);
+    		}
+    		bw.newLine();
+    	}
+        bw.flush();
+        bw.close();
+    }
+}
