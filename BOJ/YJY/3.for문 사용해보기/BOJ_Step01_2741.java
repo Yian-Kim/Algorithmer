9b9c85e85bb27c012bd9f853cf97e61fd57af943
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 19:31:43 +0900

    Update BOJ_Step01_2741.java

--- BOJ/YJY/3.for문 사용해보기/BOJ_Step01_2741.java
+++ BOJ/YJY/3.for문 사용해보기/BOJ_Step01_2741.java
@@ -2,7 +2,7 @@
 - 파일명 : BOJ_Step01_2741.java
 - 작성자 : YJY
 - 작성일자 : 2019.05.23.
-- 문제 : BOJ 1단계 사칙연산 도전하기 
+- 문제 : BOJ 1단계 for
         2741번 N 찍기 (https://www.acmicpc.net/problem/2741)
 */
 import java.io.*;
