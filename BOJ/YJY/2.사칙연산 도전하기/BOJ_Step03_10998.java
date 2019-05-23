a38957e034ed769cf70e2fa651596a43803fa1bd
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 19:18:33 +0900

    Update BOJ_Step03_10998.java

--- BOJ/YJY/2.사칙연산 도전하기/BOJ_Step03_10998.java
+++ BOJ/YJY/2.사칙연산 도전하기/BOJ_Step03_10998.java
@@ -2,8 +2,8 @@
 - 파일명 : BOJ_Step03_10998.java
 - 작성자 : YJY
 - 작성일자 : 2019.05.23.
-- 문제 : BOJ 3단계 입/출력 받아보기 
-        10998번 등록 (https://www.acmicpc.net/problem/10998)
+- 문제 : BOJ 3단계 사칙연산 도전하기 
+        10998번 A×B (https://www.acmicpc.net/problem/10998)
 */
 import java.io.*;
 import java.util.*;
