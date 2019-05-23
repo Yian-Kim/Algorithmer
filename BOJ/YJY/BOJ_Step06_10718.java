57f0a6abdd7ca936517a3697a261dc08be8a63d3
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 18:57:41 +0900

    ADD FILE [BOJ_Step06_10718.java]

---
+++ BOJ/YJY/BOJ_Step06_10718.java
@@ -0,0 +1,15 @@
+/*
+- 파일명 : BOJ_Step06_10718.java
+- 작성자 : YJY
+- 작성일자 : 2019.05.23.
+- 문제 : BOJ 6단계 입/출력 받아보기 
+        10718번 등록 (https://www.acmicpc.net/problem/10718)
+*/
+public class Main {
+    
+    public static void main(String args[]){
+
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }
+}
