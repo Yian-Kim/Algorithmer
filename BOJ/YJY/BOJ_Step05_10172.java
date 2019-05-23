62f99fa89bb464614ef057fdd87bf74036d8dc6c
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 18:55:50 +0900

    ADD FILE [BOJ_Step05_10172.java]

---
+++ BOJ/YJY/BOJ_Step05_10172.java
@@ -0,0 +1,18 @@
+/*
+- 파일명 : BOJ_Step05_10172.java
+- 작성자 : YJY
+- 작성일자 : 2019.05.23.
+- 문제 : BOJ 5단계 입/출력 받아보기 
+        10172번 등록 (https://www.acmicpc.net/problem/10172)
+*/
+public class Main {
+    
+    public static void main(String args[]){
+        
+        System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+    }
+}
