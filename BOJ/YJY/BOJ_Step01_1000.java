ae0b9c3e441388d6b97f16333c624ed569ce10e4
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 18:37:51 +0900

    Create BOJ_Step01_1000.java

---
+++ BOJ/YJY/BOJ_Step01_1000.java
@@ -0,0 +1,23 @@
+/*
+- 파일명 : BOJ_Step01_1000.java
+- 작성자 : YJY
+- 작성일자 : 2019.05.23.
+- 문제 : BOJ 1단계 입/출력 받아보기 
+        1000번 A+B (https://www.acmicpc.net/problem/1000)
+*/
+
+import java.util.*;
+
+public class Main{
+    
+    public static void main(String args[]){
+        
+        Scanner scan = new Scanner(System.in);
+        
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        
+        System.out.println(a+b);
+        
+    }        
+}
