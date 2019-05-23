b413a6bc1ae3d255d34d82fe9eee2d947f6e2422
woojoovove <47964928+woojoovove@users.noreply.github.com>
2019-05-23 18:41:48 +0900

    Create BOJ_Step27_11478.java

---
+++ BOJ/JWJ/BOJ_Step27_11478.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class ex_11478 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		String input = scan.nextLine();
+		
+		Set<String> set = new HashSet<String>();
+		
+		for(int i=0; i<input.length(); i++) {
+			for (int j=i; j<input.length(); j++) {
+				
+				set.add(input.substring(i, j+1));
+			}
+		}
+		System.out.println(set.size());
+	}
+}
