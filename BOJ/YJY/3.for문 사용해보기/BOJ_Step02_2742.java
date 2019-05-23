01feb573b9a31a2cc88264d16bc17c860c121154
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 19:29:22 +0900

    Create BOJ_Step02_2742.java

---
+++ BOJ/YJY/3.for문 사용해보기/BOJ_Step02_2742.java
@@ -0,0 +1,27 @@
+/*
+- 파일명 : BOJ_Step02_2742.java
+- 작성자 : YJY
+- 작성일자 : 2019.05.23.
+- 문제 : BOJ 2단계 사칙연산 도전하기 
+        2742번 기찍 N (https://www.acmicpc.net/problem/2741)
+*/
+import java.io.*;
+
+public class Main {
+    
+    public static void main(String args[]) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        String a;
+        int n  = Integer.parseInt(br.readLine());
+        for(int i=n; i>0; i--) {
+            a = i+"";
+            bw.write(a);
+            bw.newLine();
+        }
+        bw.flush();
+        bw.close();
+    }
+}
