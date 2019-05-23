7c2bcf1c671f83e74720f9db1d5ff4592ac212ee
jubi93 <42908491+jubi93@users.noreply.github.com>
2019-05-23 19:15:52 +0900

    Update BOJ_Step06_10718.java

--- BOJ/YJY/1.입-출력 받아보기/BOJ_Step06_10718.java
+++ BOJ/YJY/1.입-출력 받아보기/BOJ_Step06_10718.java
@@ -3,7 +3,7 @@
 - 작성자 : YJY
 - 작성일자 : 2019.05.23.
 - 문제 : BOJ 6단계 입/출력 받아보기 
-        10718번 등록 (https://www.acmicpc.net/problem/10718)
+        10718번 We love kriii (https://www.acmicpc.net/problem/10718)
 */
 public class Main {
     
