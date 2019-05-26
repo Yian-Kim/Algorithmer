9013673839f8268ced379e92c04b8b110be5ac82
Chanmi Kim <chanmi_dev@naver.com>
2019-05-26 21:34:21 +0900

    ADD FILE [BOJ_Step01_7287.java]
    - 파일명 : BOJ_Step01_7287.java
    - 작성자 : KCM
    - 작성일자 : 2019.04.26.
    - 문제 : BOJ 1단계 입/출력 받아보기
            7287번 등록 (https://www.acmicpc.net/problem/7287)
    - 난이도 : ★☆☆☆☆
    - 후기 : 이 문제는 스페셜 저지이기 때문에 어떤 값을 입력해도 성공으로 뜸

---
+++ BOJ/KCM/BOJ_Step01_7287.java
@@ -0,0 +1,20 @@
+/*
+
+- 파일명 : BOJ_Step01_7287.java
+- 작성자 : KCM
+- 작성일자 : 2019.04.26.
+- 문제 : BOJ 1단계 입/출력 받아보기
+        7287번 등록 (https://www.acmicpc.net/problem/7287)
+- 난이도 : ★☆☆☆☆
+- 후기 : 이 문제는 스페셜 저지이기 때문에 어떤 값을 입력해도 성공으로 뜸
+
+*/
+
+public class Main {
+    public static void main(String[] args) {
+ 
+    	System.out.println("3");
+    	System.out.println("aimer94");
+    	
+    }
+}
