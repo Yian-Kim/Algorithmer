e42156c76e71f7cdd84b022bb9c6aede48376043
Chanmi Kim <chanmi_dev@naver.com>
2019-05-26 21:43:14 +0900

    ADD FILE [BOJ_Step01_10718.java]
    - 파일명 : BOJ_Step01_10718.java
    - 작성자 : KCM
    - 작성일자 : 2019.04.27.
    - 문제 : BOJ 1단계 입/출력 받아보기
            10718번 We love kriii (https://www.acmicpc.net/problem/10718)
    - 난이도 : ★☆☆☆☆
    - 후기 : 한 줄씩 출력하면 매우 쉬운 문제

---
+++ BOJ/KCM/BOJ_Step01_10718.java
@@ -0,0 +1,20 @@
+/*
+
+- 파일명 : BOJ_Step01_10718.java
+- 작성자 : KCM
+- 작성일자 : 2019.04.27.
+- 문제 : BOJ 1단계 입/출력 받아보기
+        10718번 We love kriii (https://www.acmicpc.net/problem/10718)
+- 난이도 : ★☆☆☆☆
+- 후기 : 한 줄씩 출력하면 매우 쉬운 문제  
+        
+*/
+
+public class Main {
+    public static void main(String[] args) {
+ 
+    	System.out.println("강한친구 대한육군");
+    	System.out.println("강한친구 대한육군");
+
+    }
+}
