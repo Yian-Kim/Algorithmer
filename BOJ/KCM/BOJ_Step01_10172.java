5ad12e7f74472735dd723811308873713336ed63
Chanmi Kim <chanmi_dev@naver.com>
2019-05-26 21:40:16 +0900

    ADD FILE [BOJ_Step01_10172.java]
    - 파일명 : BOJ_Step01_10172.java
    - 작성자 : KCM
    - 작성일자 : 2019.04.26.
    - 문제 : BOJ 1단계 입/출력 받아보기
            10172번 개 (https://www.acmicpc.net/problem/10172)
    - 난이도 : ★☆☆☆☆
    - 후기 : 한꺼번에 출력하면 "출력 형식이 잘못되었습니다" 문구가 뜸
            제대로 출력하려면 한줄씩 출력해야함

---
+++ BOJ/KCM/BOJ_Step01_10172.java
@@ -0,0 +1,23 @@
+/*
+
+- 파일명 : BOJ_Step01_10172.java
+- 작성자 : KCM
+- 작성일자 : 2019.04.26.
+- 문제 : BOJ 1단계 입/출력 받아보기
+        10172번 개 (https://www.acmicpc.net/problem/10172)
+- 난이도 : ★☆☆☆☆
+- 후기 : 한꺼번에 출력하면 "출력 형식이 잘못되었습니다" 문구가 뜸
+        제대로 출력하려면 한줄씩 출력해야함    
+        
+*/
+
+public class Main {
+    public static void main(String[] args) {
+ 
+    	System.out.println("|\\_/|");
+    	System.out.println("|q p|   /}");
+    	System.out.println("( 0 )\"\"\"\\");
+    	System.out.println("|\"^\"`    |");
+    	System.out.print("||_/=\\\\__|");
+    }
+}
