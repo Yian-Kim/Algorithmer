f34d540cfc1c956cc1b7e648cde6e694e25be3d8
Chanmi Kim <chanmi_dev@naver.com>
2019-06-30 23:08:21 +0900

    Add File [BOJ_Step01_2557.kt]
    - 파일명 : BOJ_Step01_2557.kt
    - 작성자 : KCM
    - 작성일자 : 2019.06.30.
    - 문제 : BOJ 1단계 입출력과 사칙연산
            2557번 Hello World (https://www.acmicpc.net/problem/2557)
    - 난이도 : ★☆☆☆☆
    - 후기 : 코틀린(Kotlin)으로 처음 작성해보는 출력문입니다.

---
+++ BOJ/KCM/[01단계] 입·출력 받아보기/BOJ_Step01_2557.kt
@@ -0,0 +1,15 @@
+/*
+
+- 파일명 : BOJ_Step01_2557.kt
+- 작성자 : KCM
+- 작성일자 : 2019.06.30.
+- 문제 : BOJ 1단계 입출력과 사칙연산 
+        2557번 Hello World (https://www.acmicpc.net/problem/2557)
+- 난이도 : ★☆☆☆☆
+- 후기 : 코틀린(Kotlin)으로 처음 작성해보는 출력문입니다.
+
+*/
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
