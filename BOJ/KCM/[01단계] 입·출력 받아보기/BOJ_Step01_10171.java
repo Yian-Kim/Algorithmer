e1afa485c7331f0654a24b233c468c7561a054ff
Chanmi Kim <chanmi_dev@naver.com>
2019-06-30 14:28:24 +0900

    Add File [BOJ_Step01_10171.java]
    - 파일명 : BOJ_Step01_10171.java
    - 작성자 : KCM
    - 작성일자 : 2019.06.30.
    - 문제 : BOJ 1단계 입/출력 받아보기
            10171번 고양이 (https://www.acmicpc.net/problem/10171)
    - 난이도 : ★☆☆☆☆
    - 후기 : 출력문에 \가 등장할 경우 escape 시켜주려고
            \를 한 번 더 적어야하는데, 이 부분이 헷갈렸음.
            띄어쓰기 틀려서 '출력형식이 잘못되었습니다'가 한 번 떴으나 맞춤!

---
+++ BOJ/KCM/[01단계] 입·출력 받아보기/BOJ_Step01_10171.java
@@ -0,0 +1,22 @@
+/*
+
+- 파일명 : BOJ_Step01_10171.java
+- 작성자 : KCM
+- 작성일자 : 2019.06.30.
+- 문제 : BOJ 1단계 입/출력 받아보기
+        10171번 고양이 (https://www.acmicpc.net/problem/10171)
+- 난이도 : ★☆☆☆☆
+- 후기 : 출력문에 \가 등장할 경우 escape 시켜주려고 
+        \를 한 번 더 적어야하는데, 이 부분이 헷갈렸음.
+        띄어쓰기 틀려서 '출력형식이 잘못되었습니다'가 한 번 떴으나 맞춤!
+        
+*/
+
+public class Main{
+    public static void main(String args[]) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
