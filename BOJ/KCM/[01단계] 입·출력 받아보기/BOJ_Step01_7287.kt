4e914c7a6723295dc48083ff81e079cf333a8283
Chanmi Kim <chanmi_dev@naver.com>
2019-07-01 17:34:56 +0900

    Add File [BOJ_Step01_7287.kt]
    - 파일명 : BOJ_Step01_7287.kt
    - 작성자 : KCM
    - 작성일자 : 2019.07.01.
    - 문제 : BOJ 1단계 입출력과 사칙연산 
            7287번 등록 (https://www.acmicpc.net/problem/7287)
    - 난이도 : ★☆☆☆☆
    - 후기 : 단순 출력은 매우 쉬운 편!

---
+++ BOJ/KCM/[01단계] 입·출력 받아보기/BOJ_Step01_7287.kt
@@ -0,0 +1,16 @@
+/*
+
+- 파일명 : BOJ_Step01_7287.kt
+- 작성자 : KCM
+- 작성일자 : 2019.07.01.
+- 문제 : BOJ 1단계 입출력과 사칙연산 
+        7287번 등록 (https://www.acmicpc.net/problem/7287)
+- 난이도 : ★☆☆☆☆
+- 후기 : 단순 출력은 매우 쉬운 편!
+
+*/
+
+fun main(args: Array<String>) {
+    println("43")
+    println("aimer94")
+} 
