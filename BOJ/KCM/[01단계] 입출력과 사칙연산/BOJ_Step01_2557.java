4d77af66093ef2a255fecee8a416a0a2268ff69a
Chanmi Kim <chanmi_dev@naver.com>
2019-07-12 13:52:04 +0900

    Modify File [BOJ_Step01_2557.java]
    1. java 파일 수정
    
    - BOJ_Step01_2557.java
    - 주석 수정

--- BOJ/KCM/[01단계] 입출력과 사칙연산/BOJ_Step01_2557.java
+++ BOJ/KCM/[01단계] 입출력과 사칙연산/BOJ_Step01_2557.java
@@ -3,7 +3,7 @@
 - 파일명 : BOJ_Step01_2557.java
 - 작성자 : KCM
 - 작성일자 : 2019.04.20.
-- 문제 : BOJ 1단계 입/출력 받아보기 
+- 문제 : BOJ 1단계 입출력과 사칙연산 
         2557번 Hello World (https://www.acmicpc.net/problem/2557)
 - 난이도 : ★☆☆☆☆
 - 후기 : Baekjoon Online Judge 사이트 특성상 작성한 소스코드를 제출하려면 
