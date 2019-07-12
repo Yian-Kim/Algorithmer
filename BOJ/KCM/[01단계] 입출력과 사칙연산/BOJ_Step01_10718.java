316ecdb14c9f4d38510c04586ff2afdbbc03b94d
Chanmi Kim <chanmi_dev@naver.com>
2019-07-12 13:53:55 +0900

    Modify File [BOJ_Step01_10718.java]
    1. java 파일 수정
    
    - BOJ_Step01_10718.java
    - 주석 수정

--- BOJ/KCM/[01단계] 입출력과 사칙연산/BOJ_Step01_10718.java
+++ BOJ/KCM/[01단계] 입출력과 사칙연산/BOJ_Step01_10718.java
@@ -3,7 +3,7 @@
 - 파일명 : BOJ_Step01_10718.java
 - 작성자 : KCM
 - 작성일자 : 2019.04.27.
-- 문제 : BOJ 1단계 입/출력 받아보기
+- 문제 : BOJ 1단계 입출력과 사칙연산
         10718번 We love kriii (https://www.acmicpc.net/problem/10718)
 - 난이도 : ★☆☆☆☆
 - 후기 : 한 줄씩 출력하면 매우 쉬운 문제  
