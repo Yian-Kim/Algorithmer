ac1f7d15c6f04823c4116b660e041be993fc8353
Chanmi Kim <chanmi_dev@naver.com>
2019-07-13 13:04:28 +0900

    Add File [BOJ_Step03_10950.java]
    - 파일명 : BOJ_Step03_10950.java
    - 작성자 : KCM
    - 작성일자 : 2019.07.13.
    - 문제 : BOJ 3단계 for문
            10950번  (https://www.acmicpc.net/problem/10950)
    - 난이도 : ★☆☆☆☆
    - 후기 : 입력받는 문자열을 반복적으로 저장하는 방법만 알면 금방 푸는 문제입니다!

---
+++ BOJ/KCM/[03단계] for문/BOJ_Step03_10950.java
@@ -0,0 +1,33 @@
+/*
+
+- 파일명 : BOJ_Step03_10950.java
+- 작성자 : KCM
+- 작성일자 : 2019.07.13.
+- 문제 : BOJ 3단계 for문
+        10950번  (https://www.acmicpc.net/problem/10950)
+- 난이도 : ★☆☆☆☆
+- 후기 : 입력받는 문자열을 반복적으로 저장하는 방법만 알면 금방 푸는 문제입니다!
+        
+*/
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		int T = Integer.parseInt(scan.nextLine());
+		int arr[] = new int[T];
+
+		for (int i = 0; i < T; i++) {
+			String str = scan.nextLine();
+			int A = Integer.parseInt(str.split(" ")[0]);
+			int B = Integer.parseInt(str.split(" ")[1]);
+			arr[i] = A + B;
+		}
+
+		for (int i = 0; i < T; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+}
