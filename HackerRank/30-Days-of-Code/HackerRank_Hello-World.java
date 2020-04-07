8cf3db3a21cb3c1dc30149ae01d117720c89eb7f
Chanmi Kim <chanmi_dev@naver.com>
2020-04-07 22:10:34 +0900

    feat: Create HackerRank_Hello-World.java

---
+++ HackerRank/30-Days-of-Code/HackerRank_Hello-World.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String inputString = scan.nextLine();
+
+        scan.close();
+
+        System.out.println("Hello, World.");
+        
+        System.out.println(inputString);
+    }
+}
