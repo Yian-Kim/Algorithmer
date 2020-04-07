b21e21893c6f9cbae7ade24858cd3867251ae5fd
Chanmi Kim <chanmi_dev@naver.com>
2020-04-07 23:40:44 +0900

    fix: Fix typo

--- HackerRank/30-Days-of-Code/HackerRank_Day3_Intro-to-Conditional-Statements.java
+++ HackerRank/30-Days-of-Code/HackerRank_Day3_Intro-to-Conditional-Statements.java
@@ -5,21 +5,13 @@ public class Solution {
     static String solve(int N) {
         String result = "";
 
-        if (N >= 6 && N <= 20) {
-            result = "Weird";
-        }
+        if (N >= 6 && N <= 20) { result = "Weird"; }
 
-        if (N >= 2 && N <= 5) {
-            result = "Not Weird";
-        }
+        if (N >= 2 && N <= 5) { result = "Not Weird"; }
 
-        if (N > 20) {
-            result = "Not Weird";
-        }
+        if (N > 20) { result = "Not Weird"; }
 
-        if (N % 2 != 0) {
-            result = "Weird";
-        }
+        if (N % 2 != 0) { result = "Weird"; }
 
         return result;
     }
